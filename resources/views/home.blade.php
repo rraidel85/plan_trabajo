bdf151978b28fd7be34b056e6e1eb1fae488aea7
Yaniel Torres <yanielt@gmail.com>
2022-01-24 12:20:17 -0500

    integrated adminlte template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <p>Contenido principal</p>
+@stop
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin_custom.css') }}">
+@stop
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@section('js')
+{{--    <script> console.log('Hi!'); </script>--}}
+@stop
